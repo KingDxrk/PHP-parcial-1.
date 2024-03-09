c4ab82d7dd37a90c564633671a659854e879a862
KingDxrk <159971793+KingDxrk@users.noreply.github.com>
2024-03-08 21:58:25 -0600

    Add files via upload

---
+++ parcial 1/Trabajo1.8/tarea1.8.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problemas</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h1>Menu principal</h1>
+    <p>Aqui puedes ver 3 problemas de omegaup con su solucion</p>
+    <ul class="Menu">
+    <li class="problema1"><a href="problema1.php">4352. Bit de paridad</a></li>
+    <li><a href="problema2.php">4886. Autores</a></li>
+    <li><a href="problema3.php">3991. Adquisición de Letras</a></li>
+    </ul>
+    <h2>Angel Rafael Lopez Gonzalez</h2>
+
+    <pre style="font-size: 20px;">
+                     .'''''.
+                 _.-'/  |  \
+    ,        _.-"  ,|  /  0 `-.
+    |\    .-"       `--""-.__.'=====================-,
+    \ '-'`        .___.--._)=========================|
+     \            .'      |                          |
+      |     /,_.-'        |     06/marzo/2024        |
+    _/   _.'(             |     Angel                |
+   /  ,-' \  \            |            Rafael        |
+   \  \    `-'            |     Lopez                |
+    `-'                   |             Gonzalez     |               
+                          _____________________________
+</pre>
+</body>
+</html>
